50ec51e1ed4c24bd10bd5e45fa41e48dadbe5a66
Arif Khan <Arif@sparta.com>
2025-03-11 15:31:45 +0000

    routes are commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Product from "./pages/Product"
 import ProductDetail from "./pages/ProductDetail"
+import Card from "./pages/Card"
+import Address from "./pages/Address"
+import Checkout from "./pages/Checkout"
+import Gallery from "./pages/Gallery"
 
  
 function App() {
@@ -14,6 +18,11 @@ function App() {
        <Routes>
           <Route index element={<Home/>} />
           <Route path="/about-us" element={<About/>} />
+          <Route path="/contact-us" element={<Contact/>} />
+          <Route path="/my-card" element={<Card/>} />
+          <Route path="/my-address" element={<Address/>} />
+          <Route path="/checkout" element={<Checkout/>} />
+          <Route path="/gallery" element={<Gallery/>} />
           <Route path="/contact-us" element={<Contact/>} />
           <Route path="/product" element={<Product/>} />
           <Route path="/product-details" element={<ProductDetail/>} />
