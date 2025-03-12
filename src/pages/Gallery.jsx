7590d77e9a874fcc0297ca8307aba3af924cf873
vani000786 <vani.mendiratta@fastranking.tech>
2025-03-12 15:39:30 +0530

    all data commited

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Tab from'./components/Tab'
+import Header from './components/Header'
+import Footer from'./components/Footer'
 
 const Gallery = () => {
   return (
     <div>
-      
+      <div
+        className="h-[400px] mb-4 bg-cover bg-center flex justify-center items-center w-full overflow-hidden"
+        style={{
+          backgroundImage: "url('https://res.cloudinary.com/dj7wogsju/image/upload/v1741711262/Group_238820_fsqqzy.png')",
+        }}
+        id="Hero"
+      >
+        <h2 className="text-center text-white text-[80px] font-[400]">About Us</h2>
+      </div>
+<Header/>
+      <Tab/>
+      <Footer/>
     </div>
   )
 }
